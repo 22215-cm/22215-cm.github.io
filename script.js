a4117e1c8f0729c28e1e4473665a000c1927b5e8
22215-cm <yakhin.aynur@list.ru>
2025-02-11 18:58:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+const burgerMenu = document.getElementById('burgerMenu');
+const navLinks = document.getElementById('navLinks');
+
+burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active'); // Переключаем класс "active"
+});
